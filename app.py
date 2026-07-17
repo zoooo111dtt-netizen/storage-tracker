91016ba8475c708ca6bae6ac8f18b55892a9c692
zoooo111dtt-netizen <zoooo111dtt@gmail.com>
2026-07-17 12:07:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ if "last_data_len" not in st.session_state or st.session_state.last_data_len != 
         people_agg = df_calc.groupby("打卡人")["释放空间(GB)"].sum().reset_index()
         st.session_state.rank_all = people_agg.sort_values(by="释放空间(GB)", ascending=False)
         st.session_state.rank_week = this_week_users.groupby("打卡人")["释放空间(GB)"].sum().reset_index().sort_values(by="释放空间(GB)", ascending=False) if not this_week_users.empty else pd.DataFrame()
-        st.session_state.rank_month = this_month_users.groupby("打卡人")["释放空間(GB)"].sum().reset_index().sort_values(by="释放空间(GB)", ascending=False) if not this_month_users.empty else pd.DataFrame()
+        st.session_state.rank_month = this_month_users.groupby("打卡人")["释放空间(GB)"].sum().reset_index().sort_values(by="释放空间(GB)", ascending=False) if not this_month_users.empty else pd.DataFrame()
         
         # 3. 图表数据源预处理
         trend_grouped = df_calc.groupby(["日期显示", "打卡人"])["释放空间(GB)"].sum().reset_index().sort_values(by="日期显示")
